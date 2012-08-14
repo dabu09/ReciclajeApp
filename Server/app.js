b399ea59e5cb65052ffdbed621cde3309df0eee5
Rolando Perez <rolilink@localhost.localdomain>
2012-08-13 23:43:20 -0500

    Conexion a MongoDB Lista

--- Server/app.js
+++ Server/app.js
@@ -1,18 +1,37 @@
 //Modules
 var express = require('express');
 var mongoose = require('mongoose');
-
 var app = express();
 
-//Config
-app.configure(function(){
-	//configurations goes here
+//on connection sucess
+mongoose.connection.on('open',function(ref){
+	app.set('env','development');
+	//app.set('env','production');
+	app.configure(function(){
+		//standar configs
+
+		//development configs
+		if(app.get('env')=='development'){
+
+		}
+		//production configs
+		if(app.get('env')=='production'){
+
+		}
+	});
+	//routes
+	app.get('/',function(req,res){
+		res.send('Hello World');
+	});
+	app.listen(3000);
+	console.log("Server Up And Running");
 });
 
+//on connection error
 
-app.get('/',function(req,res){
-	res.send('Hello World');
+mongoose.connection.on('error',function(error){
+	console.log('Could not connect to db');
+	console.log('Error:'+error);
 });
 
-app.listen(3000);
-console.log("Server Up And Running");
+var db = mongoose.connect('mongodb://localhost/test');;
